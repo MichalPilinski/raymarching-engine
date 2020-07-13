3c1583f33f363a859918965b386c42360ecffca5
Michał Piliński <mpilinski@protonmail.com>
2020-07-13 22:28:44 +0200

    Added camera class

---
+++ raymarching/raymarching/ComputationClasses/Camera.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+
+namespace raymarching.ComputationClasses
+{
+    class Camera
+    {
+        private Vector3 CameraPosition { get; set; }
+        private Vector3 CameraTarget { get; set; }
+
+        private Vector2 SensorSize;
+        private Vector2 Resolution;
+        private float FocalLength;
+        private float ViewTensorsAngle;
+
+        private Vector3 CameraVector;
+        private Vector3 SensorYTensor, SensorXTensor;
+        private readonly Vector3 XTensor = new Vector3(0, 0, 1);
+
+        Camera(Vector2 _sensorSize, Vector2 _resolution, float _focalLength, float _viewTensorAngle)
+        {
+            CameraPosition = new Vector3();
+            CameraTarget = new Vector3(1, 1, 1);
+
+            SensorSize = _sensorSize;
+            Resolution = _resolution;
+            FocalLength = _focalLength;
+            ViewTensorsAngle = _viewTensorAngle;
+        }
+
+        private void UpdateLocalCoords()
+        {
+            SensorXTensor = Vector3.Normalize(Vector3.Cross(XTensor, CameraVector));
+            SensorYTensor = Vector3.Normalize(Vector3.Cross(SensorXTensor, CameraVector));
+        }
+
+        private void UpdateCameraVector()
+        {
+            CameraVector = Vector3.Normalize(CameraTarget - CameraPosition) * FocalLength;
+        }
+
+        private void RotateViewTensors(float angle)
+        {
+            Quaternion RotationMatrix = new Quaternion(CameraVector, angle);
+
+            Vector3.Transform(SensorYTensor, RotationMatrix);
+            Vector3.Transform(SensorXTensor, RotationMatrix);
+        }
+
+        public void UpdateCamera()
+        {
+            UpdateLocalCoords();
+            RotateViewTensors(ViewTensorsAngle);
+            UpdateCameraVector();
+        }
+
+        public Vector3 GetRayDirection(int PosX, int PosY)
+        {
+            Vector3 SensorGlobalX = SensorSize.X * SensorXTensor * (PosX / Resolution.X - 0.5f);
+            Vector3 SensorGlobalY = SensorSize.Y * SensorYTensor * (PosY / Resolution.Y - 0.5f);
+
+            return CameraVector + SensorGlobalX + SensorGlobalY;
+        }
+    }
+}
--- raymarching/raymarching/MainWindow.xaml.cs
+++ raymarching/raymarching/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media;
 
 namespace raymarching
 {
@@ -20,9 +23,63 @@ namespace raymarching
     /// </summary>
     public partial class MainWindow : Window
     {
+        WriteableBitmap TempBitmap { set; get; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TempBitmap = new WriteableBitmap((int)this.Width, (int)this.Height, 96, 96, System.Windows.Media.PixelFormats.Bgr32, null);
+
+            Canvas.MouseMove += new MouseEventHandler(RenderLoopEvent);
+
+            Canvas.Source = TempBitmap;
+            Canvas.Stretch = Stretch.None;
+            Canvas.HorizontalAlignment = HorizontalAlignment.Left;
+            Canvas.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        protected void RenderLoopEvent(object sender, EventArgs e)
+        {
+            DrawRandomData(TempBitmap);
+        }
+
+        private void DrawRandomData(WriteableBitmap bitmap)
+        {
+            try
+            {
+                bitmap.Lock();
+
+                unsafe
+                {
+                    IntPtr pBackBuffer = bitmap.BackBuffer;
+                    int RandomNum = new Random().Next(255);
+
+                    for (int i = 0; i < bitmap.PixelHeight; i++)
+                    {
+                        for (int j = 0; j < bitmap.PixelWidth; j++)
+                        {
+                            pBackBuffer += 4;
+
+                            // Compute the pixel's color.
+                            int color_data = 0 << 16; // R
+                            color_data |= 0 << 8;   // G
+                            color_data |= 0 << 0;   // B
+
+                            // Assign the color data to the pixel.
+                            *((int*)pBackBuffer) = color_data;
+                        }
+                    }
+                }
+
+                // Specify the area of the bitmap that changed.
+                bitmap.AddDirtyRect(new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+            }
+            finally
+            {
+                // Release the back buffer and make it available for display.
+                bitmap.Unlock();
+            }
         }
     }
 }
